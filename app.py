767bbf3188b1a83434a8104904490f675eab2531
AryanHande13 <144030425+AryanHande13@users.noreply.github.com>
2025-02-11 00:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,10 @@
-import os
 import pickle
-import streamlit as st
+import streamlit as st 
 from streamlit_option_menu import option_menu
 
-# Set page configuration
-st.set_page_config(page_title="Health Assistant",
-                   layout="wide",
-                   page_icon="🧑‍⚕️")
-
-    
-# getting the working directory of the main.py
-working_dir = os.path.dirname(os.path.abspath(__file__))
-
-# loading the saved models
-
-diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
-
-heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
-
-parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
+diabetes_model=pickle.load(open("diabetes_model.sav",'rb'))
+heart_disease_model=pickle.load(open("Heart_model.sav",'rb'))
+parkinsons_model=pickle.load(open("parkinsons_model.sav",'rb'))
 
 # sidebar for navigation
 with st.sidebar:
